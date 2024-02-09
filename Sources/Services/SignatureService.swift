0f282e30acf96d9f17409ad74094cc80b07ab270
Eugene <upeugene@gmail.com>
2024-02-08 22:14:05 -0800

    Remove CommonCrypto

--- Sources/Services/SignatureService.swift
+++ Sources/Services/SignatureService.swift
@@ -5,14 +5,12 @@
 //  Created by Eugene Popov on 2/1/24.
 //
 
-import CommonCrypto
 import Foundation
 import Crypto
 
 enum SignatureError: Error {
     case noData
 }
-
 
 public final class SignatureService {
     public enum Constants {
@@ -26,10 +24,11 @@ public final class SignatureService {
     public static func generateSignature(data: String, key: String) -> String? {
         guard !key.isEmpty,
               let keyData = key.data(using: .utf8),
-              let dataToSign = data.data(using: .utf8) else { return nil }
+              let dataToSign = data.data(using: .utf8)
+        else { return nil }
         
-        var hmac = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
-        CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA256), keyData.bytes, keyData.count, dataToSign.bytes, dataToSign.count, &hmac)
+        let key = SymmetricKey(data: keyData)
+        let hmac = HMAC<SHA256>.authenticationCode(for: dataToSign, using: key)
         let signature = Data(hmac).base64EncodedString()
         return signature
     }
@@ -37,12 +36,14 @@ public final class SignatureService {
     public static func verifySignature(signature: String, data: String, key: String) throws -> Bool {
         guard
             !key.isEmpty,
-            let secretKey = key.data(using: .utf8),
-            let data = data.data(using: .utf8) else {
+            let dataCode = data.data(using: .utf8),
+            let secretKey = key.data(using: .utf8) else {
             throw SignatureError.noData
         }
         
-        let expectedHMAC = HMAC<SHA256>.authenticationCode(for: data, using: .init(data: secretKey))
+        let expectedHMAC = HMAC<SHA256>.authenticationCode(
+            for: dataCode,
+            using: SymmetricKey(data: secretKey))
         let expectedSignature = Data(expectedHMAC).base64EncodedString()
         
         return signature == expectedSignature
@@ -70,7 +71,6 @@ public final class SignatureService {
         let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
         return String((0..<length).map { _ in characters.randomElement()! })
     }
-    
 }
 
 extension Data {
